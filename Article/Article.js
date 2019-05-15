9229e1a346b9c8ba36c968ac4300c735518cbbc7
Dustin Hamano <dhamano@Dustins-iMac-Pro-2.local>
2019-05-15 15:06:09 -0700

    created a way to make articles and append them to the well

--- Article/Article.js
+++ Article/Article.js
@@ -1,6 +1,45 @@
 // Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
 
+class GenerateArticle {
+  constructor(article) {
+    this.article = article;
+    this.title = article.title;
+    this.date = article.date;
+    this.content = article.content;
+  }
+  
+  addArticle() {
+    const containerEl = document.createElement('div');
+    const closerEl = document.createElement('i');
+    const h2El = document.createElement('h2');
+    const pDate = document.createElement('p');
+    const pEl = document.createElement('p');
+    const spanEl = document.createElement('span');
+    containerEl.classList.add('article');
+    closerEl.classList.add('fas', 'fa-times', 'remove');
+    pDate.classList.add('date');
+    spanEl.classList.add('expandButton');
+    h2El.textContent = this.title;
+    pDate.textContent = this.date;
+    pEl.textContent = this.content;
+    document.querySelector('.articles').appendChild(containerEl);
+    containerEl.appendChild(h2El);
+    containerEl.appendChild(closerEl);
+    containerEl.appendChild(pDate);
+    containerEl.appendChild(pEl);
+    containerEl.appendChild(spanEl);
+    document.querySelector('.articles').prepend(containerEl);
+  }
+}
+
+const newArticle = new GenerateArticle({
+  title: "hello, world!",
+  date: "May 4th, 2019",
+  content: "Sirloin landjaeger tenderloin buffalo, pork chop chuck beef shankle. Ground round fatback chicken, kevin picanha jerky jowl spare ribs brisket turkey biltong. Pig pork loin beef ribs, boudin fatback meatloaf ham pork belly picanha brisket porchetta tri-tip pancetta tongue. Pig swine kielbasa, bacon frankfurter andouille turkey capicola brisket leberkas pork belly cow. Flank pork belly sirloin"
+})
+newArticle.addArticle();
+
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
